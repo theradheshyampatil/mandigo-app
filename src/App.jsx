5d80c92e3a1b6f19ded0b003e5a49cef772b74f9
theradheshyampatill <radheshyam9096@gmail.com>
2026-06-24 10:32:30 +0000

    feat: initial react frontend with dockerfile and ci pipeline

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>🍎 Welcome to MandiGo!</h1>
+      <p>B2B Fruit Marketplace - Infrastructure is live.</p>
+    </div>
+  );
+}
+
+export default App;
